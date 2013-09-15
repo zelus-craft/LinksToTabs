b348d75cbe8974a046809456ccefb57659f26116
Tyler Wood <zelus@zelus.me>
2013-09-15 16:42:37 -0700

    Main Javascript function to execute.
    This javascript function needs to be called and used to open external
    link tabs.

---
+++ openLinksInTabs.js
@@ -0,0 +1,35 @@
+$(document).ready(function() {
+	
+	// For error checking, I want a list of all of the links.
+	var links = document.getElementsByTagName("a"), linklist = [];
+
+	$("a[href^=http]").each(function(){
+
+		//Don't want to open host's links all the time.
+		var excludes = [
+		'feedly.com',
+		'cloud.feedly.com'
+		];
+      	
+      	for(i=0; i<excludes.length; i++) {
+      		if(this.href.indexOf(excludes[i]) != -1) {
+      			return true; // continue each() with next link
+      		}
+    	}
+
+	    if(this.href.indexOf(location.hostname) == -1) {
+
+	    	linklist.push(this.href.value);
+
+		   	$(this).click(function() { return true; }); 
+
+		   	$(this).attr({
+		   		target: "_blank",
+		        title: "new tab"
+		    });
+
+	        $(this).click();
+
+	    }
+   })
+});
